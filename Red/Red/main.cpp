18951bd03c6dba3dd737c0b143a040fa3aa3d075
Luis Sneyder Rodriguez Menjura <125145182+lsrm1@users.noreply.github.com>
2023-04-28 22:39:12 -0500

    Algoritmo Mejor Camino

--- Red/Red/main.cpp
+++ Red/Red/main.cpp
@@ -2,8 +2,52 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+void way(char router,map<char, map<char, int>> &red, char origen, char destino, int &costo, vector<char> &ruta ){
+
+    map<char, int>:: iterator m;
+    map<char, int>:: iterator p;
+    vector<char>:: iterator f;
+
+    char _router;
+
+
+    for (m = red[router].begin() ;  m != red[router].end(); m++){
+
+        f = find(ruta.begin(), ruta.end(), m ->first);
+
+        if ( f == ruta.end() ){
+
+            p = red[ m -> first ].find(destino);
+
+            if ( p != red[ m-> first ].end()){
+
+
+                ruta.push_back( m -> first);
+                ruta.push_back( p -> first);
+
+                costo += m -> second;
+                costo += p -> second;
+
+
+            }
+
+            else{
+
+                costo += m -> second;
+                _router = m -> first;
+                ruta.push_back( m -> first);
+                way( _router ,red, origen, destino, costo, ruta);
+
+            }
+
+        }
+    }
+}
+
 
 /*class Router{
     public:
@@ -20,19 +64,19 @@ Router::Router(char id, int costo){
 
 class Red{
     public:
-    map<char, map<char, int>> red;
+    map<char, map<char, int>> mapared;
 
     public:
     Red();
 
     void agregarouter( char id, map<char, int> tabla ){
-        red.insert(pair< char, map<char, int> >(id,tabla));
+        mapared.insert(pair< char, map<char, int> >(id,tabla));
     }
 
     void conectar(char id, char _id,int costo){
 
-        red[id].insert(pair<char,int>(_id,costo));
-        red[_id].insert(pair<char,int>(id,costo));;
+        mapared[id].insert(pair<char,int>(_id,costo));
+        mapared[_id].insert(pair<char,int>(id,costo));;
     }
 
  };
@@ -47,6 +91,7 @@ int main()
     char rep = 'y';
 
     Red red = Red();
+
     do{
         cout<<"id: ";cin>> id;
         Router router = Router(id,costo);
@@ -63,89 +108,118 @@ int main()
         red.conectar(id,_id,costo);
         cout<<"Agregar otro conexion?: ";cin>>rep;
 
-    }while(rep == 'y');
-
-    cout<<red.red.size();*/
+    }while(rep == 'y');*/
 
 int main(){
-
-    map<char, map<char, int>> mapmap;
-    vector<int> ney ;
-
-    // insert elements in random order
-    mapmap.insert(make_pair('A', map<char, int> {make_pair('A', 0)}));
-    mapmap['A'].insert(pair<char,int>('B', 4));
-    mapmap['A'].insert(pair<char,int>('C', 10));
-    mapmap['A'].insert(pair<char,int>('E', 15));
-    mapmap.insert(make_pair('B', map<char,int> {make_pair('B', 0)}));
-    mapmap['B'].insert(pair<char,int>('A', 4));
-    mapmap['B'].insert(pair<char,int>('C', 6));
-    mapmap['B'].insert(pair<char,int>('E', 8));
-    mapmap.insert(make_pair('C', map<char, int> {make_pair('C', 0)}));
-    mapmap['C'].insert(pair<char,int>('B', 6));
-    mapmap['C'].insert(pair<char,int>('A', 10));
-    mapmap['C'].insert(pair<char,int>('E', 2));
-    mapmap.insert(make_pair('E', map<char, int> {make_pair('E', 0)}));
-    mapmap['E'].insert(pair<char,int>('A', 10));
-    mapmap['E'].insert(pair<char,int>('C', 2));
-    mapmap['E'].insert(pair<char,int>('B', 8));
-
-    //map<char, map<char, int>>:: iterator i;
-   map<char, int>:: iterator k;
-
-   k = mapmap['A'].find('C');
-
-   if (k != mapmap['A'].end()){
-
-       ney.push_back(k -> second);
-   }
+    int costo = 0;
+    vector<char> ruta;
+    char origen = 'A';
+    char destino = 'C';
+    char router ;
+    vector <int> allway ;
+    vector <vector<char>> allruta;
+    map<char, map<char, int>> red;
+    int minimo = 1000;
 
 
-
-
-
-    //A -> C
+    red.insert(make_pair('A', map<char, int> {make_pair('A', 0)}));
+    red['A'].insert(pair<char,int>('B', 4));
+    red['A'].insert(pair<char,int>('C', 10));
+    //red['A'].insert(pair<char,int>('E', 15));
+    red['A'].insert(pair<char,int>('F', 5));
+    red.insert(make_pair('B', map<char,int> {make_pair('B', 0)}));
+    red['B'].insert(pair<char,int>('A', 4));
+    //red['B'].insert(pair<char,int>('C', 6));
+    red['B'].insert(pair<char,int>('E', 1));
+    red.insert(make_pair('C', map<char, int> {make_pair('C', 0)}));
+    //red['C'].insert(pair<char,int>('B', 6));
+    red['C'].insert(pair<char,int>('A', 10));
+    red['C'].insert(pair<char,int>('E', 2));
+     red['C'].insert(pair<char,int>('F', 1));
+    red.insert(make_pair('E', map<char, int> {make_pair('E', 0)}));
+    //red['E'].insert(pair<char,int>('A', 10));
+    //red['E'].insert(pair<char,int>('C', 2));
+    red['E'].insert(pair<char,int>('B', 1));
+     red['E'].insert(pair<char,int>('F', 1));
+    red.insert(make_pair('F', map<char, int> {make_pair('F', 0)}));
+    red['F'].insert(pair<char,int>('A', 5));
+    red['F'].insert(pair<char,int>('C', 1));
+    red['F'].insert(pair<char,int>('E', 1));
 
 
 
-   /* for (k = mapmap['A'].begin(); k !=  mapmap['A'].end(); k++){
-        cout<<"Clave ";
-        cout << k -> first<<'\n';
-        cout<<"Valor;";
-        cout<<k -> second<<'\n';
+   map<char, int>:: iterator i;
+   map<char, int>:: iterator it;
+   vector<char>:: iterator fi;
 
-    }*/
 
+    it = red[origen].find(destino);
 
 
 
+    if ( it != red[origen].end()){
+
+        ruta.push_back(origen);
+        ruta.push_back(destino);
+        allway.push_back(it -> second);
+        allruta.push_back(ruta);
+
+    }
+
+    ruta.clear();
+    ruta.push_back(origen);
 
 
+    for (it = red[origen].begin() ;  it != red[origen].end(); it++){
 
+        fi= find(ruta.begin(), ruta.end(), it ->first);
 
-/*
-    for (i = mapmap.begin(); i != mapmap.end(); i++){
+        if ( fi == ruta.end() ){
 
-        cout<<"Clave Principal ";
-        cout << i -> first<<'\n';
+           i = red[ it-> first ].find(destino);
 
+            if ( i != red[ it-> first ].end()){
 
-        for (k = i->second.begin(); k !=  i->second.end(); k++){
-            cout<<"Clave ";
-            cout << k -> first<<'\n';
-            cout<<"Valor;";
-            cout<<k -> second<<'\n';
+                ruta.push_back( it-> first);
+                ruta.push_back( i-> first);
+
+                costo = it -> second;
+                costo += i -> second;
+
+                allway.push_back(costo);
+                allruta.push_back(ruta);
+
+            }
+
+            else{
+
+                costo = it -> second;
+                router = it -> first;
+                ruta.push_back( it-> first);
+
+                way(router,red, origen, destino, costo, ruta );
+
+                allway.push_back(costo);
+                allruta.push_back(ruta);
+
+            }
+
 
         }
 
+    }
 
-        cout<<endl;
-    }*/
+    for (int l = 0 ; l < allway.size() ; l++){
+
+        if(allway[l] < minimo){
+
+         minimo = allway[l];
+         ruta = allruta[l];
+
+        }
+
+    }
 
 
-
-
-
-
- return 0;
+  return 0;
 }
